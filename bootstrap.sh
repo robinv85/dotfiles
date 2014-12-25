1a7dd70a3ba00b05720956586d4fa7d537507098
Robin Venneman <robin.venneman@gmail.com>
2014-12-25 12:52:15 +0100

    added Tmux.conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 dir=`pwd`
 
 # List of files & folders to symlink
-list="bashrc profile bash_profile bash_prompt vim vimrc osx editorconfig"
+list="bashrc profile bash_profile bash_prompt vim vimrc osx editorconfig tmux.conf"
 
 # Create symlinks
 link_dots() {
