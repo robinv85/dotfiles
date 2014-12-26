86755f103b5688a5167963944da1b094aa07072e
Robin Venneman <robin.venneman@gmail.com>
2014-12-26 13:07:42 +0100

    Updated bash script according to shellcheck feedback

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,32 +11,28 @@
 cd "$(dirname "${BASH_SOURCE}")"
 
 # Get script path
-dir=`pwd`
+dir=$(pwd)
 
 # List of files & folders to symlink
 list="bashrc profile bash_profile bash_prompt vim vimrc osx editorconfig tmux.conf"
 
 # Create symlinks
-link_dots() {
+link_dotfiles() {
   echo "Linking dotfiles"
   for item in $list; do
-    if [ -e ~/.$item ]
+    if [ -e ~/."$item" ]
     then
       echo "item (file/folder) exists - creating ~/backup/.$item"
       [ -d ~/backup ] ||  mkdir ~/backup
-      mv ~/.$item ~/backup/.$item
+      mv ~/."$item" ~/backup/."$item"
     else
       echo "creating ~/.$item"
     fi
-    ln -F -s -v $dir/$item ~/.$item
+    ln -F -s -v "$dir"/"$item" ~/."$item"
   done
 }
 
-# TODO : improve symlinking & file structure e.g. using .symlink extension
-# TODO : setup ?
-# TODO : create command with options: overwrite, backup, uninstall, update
-
-link_dots
+link_dotfiles
 
 # Install vim bundles via Vundle
 vim +BundleInstall +qall
